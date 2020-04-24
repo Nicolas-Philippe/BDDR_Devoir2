7ba8a8d15a98978a14c631b9ebdc0e10a1a4fc7c
Nicolas-Philippe <43750240+Nicolas-Philippe@users.noreply.github.com>
2020-04-24 12:11:05 -0400

    draft index inversé

--- build.sbt
+++ build.sbt
@@ -2,4 +2,11 @@ name := "Devoir2_BDDR"
 
 version := "0.1"
 
-scalaVersion := "2.12.10"
+scalaVersion := "2.12.0"
+
+updateOptions := updateOptions.value.withCachedResolution(true)
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.4"
+libraryDependencies += "org.apache.spark" %% "spark-graphx" % "2.4.4"
+
+useCoursier := false
--- src/main/scala/testGit/scala.scala
+++ src/main/scala/testGit/scala.scala
@@ -1,5 +1,37 @@
 package testGit
 
-class scala {
+import java.util
 
+import org.apache.spark.SparkConf
+
+object scala extends App {
+
+  val conf = new SparkConf()
+    .setAppName("AAAAAAAAAAAAAAAAAh")
+    .setMaster("local[*]")
+  val sc = new org.apache.spark.SparkContext(conf)
+  sc.setLogLevel("ERROR")
+
+
+  // Créa -> sort1, sort2
+
+  class Creature(var name: String = "", var spellList: Array[String]) extends java.io.Serializable {}
+  var creatureArray = new Array[Creature](3)
+  creatureArray(0) = new Creature("Archange de lumière", Array("Bénédiction", "Résurrection"))
+  creatureArray(1) = new Creature("Prêtre", Array("Bénédiction", "Soin"))
+  creatureArray(2) = new Creature("Mage", Array("Soin", "Invocation d'un familier", "Boule de feu"))
+  val result = sc.makeRDD(creatureArray)
+  // faudrait avoir: (1, A), (2, A)
+  //                 (2, B), (3, B)
+  //                 (1, C), (3, C)
+  result.map(el => (el.spellList.last, el.name))
+
+  var newResult = result.flatMap(crea => {
+    //var list = Set[Tuple2]()
+    var list : List[(String,String)] = List()
+    crea.spellList.foreach(spell => list = list:+((spell, crea.name)))
+    list
+  })
+  var latestResult = newResult.reduceByKey((accum, n) => (accum + " | " + n))
+  latestResult.foreach(el => println(el._1 + " => " + el._2))
 }
